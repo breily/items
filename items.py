68381209009fbd62abef3b5b08e7745d19dfa175
Brian Reily <brian@brianreily.com>
2009-02-11 04:17:45 -0500

    * I broke it for now, but cool changes are in progress. * Too much mucking about in object's __stuff makes your brain hurt.

--- items.py
+++ items.py
@@ -5,18 +5,27 @@ from sqlalchemy.orm import sessionmaker, mapper
 
 class ModelError(Exception): pass
 
+_session = None
+
 class Items(object):
     def __init__(self, engine_name='sqlite:///:memory:'):    
         self.engine = create_engine(engine_name)
         self.session = sessionmaker(bind=self.engine)()
         self.models = {}
+        global _session, _hub
+        _session = self.session
+        _hub = self
 
     def model(self, model_name, **kwargs):
         # Start building the __dict__ for the class
         # These functions defined below
         cls_dict = {'__init__': init_func,
                     '__name__': model_name,
-                    '__repr__': repr_func 
+                    '__repr__': repr_func,
+                    '__self__': None,
+                    'session':  self.session,
+                    'find':     None,
+                    'hub':      self,
                    }
         # Automatically include an 'id' column
         cols = [ Column('id', Integer, primary_key=True), ]
@@ -35,6 +44,14 @@ class Items(object):
             raise ModelError("Must have at least one column other than id")
         # Create the class
         new_model = ModelConstructor(model_name, (object,), cls_dict)
+        # Wrap the find function in a lambda so that we can pass in the class
+        # now, instead of needing to do that later.  Make sure the lambda
+        # becomes a staticmethod so we can do Person.find(), instead of p.find()
+        new_model.find = staticmethod(lambda: find_func(new_model))
+
+        #new_model.__getattribute__ = lambda attr: attr_func(new_model, attr)
+        #new_model.__getattr__ = staticmethod(lambda attr: attr_func(new_model, attr))
+
         # Setup the table
         metadata = MetaData()
         new_table = Table(model_name + 's', metadata, *cols)
@@ -82,10 +99,35 @@ column_mapping = {'string':      String,      'str':      String,
 
 # Generic __init__ to set instance variables of a class.
 def init_func(self, **kwargs):
+    print self.__dict__.items()
     for key, val in kwargs.items():
         self.__dict__[key] = val
+    print self.__dict__.items()
+
 
 # Generic __repr__ to print the class name and database id
 def repr_func(self):
     return '<%s: %s>' %(self.__name__, self.id)
 
+# Uses global session value to get a query object for the class passed
+# to the function.
+def find_func(cls):
+    return _session.query(cls)
+
+def attr_func(self, attr):
+    q = self.find()
+    print 'self: %s' %self
+    print 'attr: %s' %attr
+    print 'q: %s' %q
+    return q.__getattribute__(attr)
+    #return lambda: q.__getattribute__(attr)
+
+"""
+Maybe make the model into the session?
+    Book.add(a, b, c)
+    Book.commit()
+    Book.all()
+    Book.filter(Book.name.like="Fight%")
+
+Then get rid of the Items controller.
+"""
